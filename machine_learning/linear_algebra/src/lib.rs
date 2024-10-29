1fb635ffec060efb013e32f7e1ef36a37af38d17
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-10-29 21:36:12 +0600

    Supported vector corresponding sum

--- machine_learning/linear_algebra/src/lib.rs
+++ machine_learning/linear_algebra/src/lib.rs
@@ -26,6 +26,23 @@ pub fn subtract(v: &Vector, w: &Vector) -> Option<Vector> {
     Some(sub)
 }
 
+pub fn vector_sum(vectors: &Vec<&Vector>) -> Option<Vec<f64>> {
+    if vectors.iter().all(|v| v.len() == vectors[0].len()) {
+        let mut corresponding_sum: Vec<f64> = Vec::new();
+        for i in 0..vectors[0].len() {
+            let mut sum: f64 = 0.;
+            for v in vectors {
+                sum += v[i];
+            }
+            corresponding_sum.push(sum);
+        }
+
+        return Some(corresponding_sum);
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -46,5 +63,14 @@ mod test {
 
         assert_eq!(subtract(&vec![3.], &vec![-3.]), Some(vec![6.]));
         assert_eq!(subtract(&vec![1.], &vec![4.]), Some(vec![-3.]));
+    }
+
+    #[test]
+    fn test_vector_corresponding_sum() {
+        assert_eq!(vector_sum(&vec![&vec![1., 2.], &vec![3.]]), None);
+        assert_eq!(vector_sum(&vec![&vec![4.], &vec![]]), None);
+
+        assert_eq!(vector_sum(&vec![&vec![1.], &vec![3.]]), Some(vec![4.]));
+        assert_eq!(vector_sum(&vec![&vec![3.], &vec![6.]]), Some(vec![9.]));
     }
 }
