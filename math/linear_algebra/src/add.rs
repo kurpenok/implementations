b7d12d03b274997c35da52ed53cc14b445fe90b7
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-01 18:06:47 +0600

    Supported various types of vector operations

---
+++ math/linear_algebra/src/add.rs
@@ -0,0 +1,26 @@
+use std::ops::Add;
+
+pub fn add<T: Copy + Add<Output = T>>(v: &[T], w: &[T]) -> Option<Vec<T>> {
+    if v.len() != w.len() {
+        return None;
+    }
+
+    Some(v.iter().zip(w.iter()).map(|(&x, &y)| x + y).collect())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_vector_addition() {
+        assert_eq!(add(&vec![], &vec![-3., 2.]), None);
+        assert_eq!(add(&vec![1., 2.], &vec![4.]), None);
+
+        assert_eq!(add(&vec![1, 2], &vec![4, 5]), Some(vec![5, 7]));
+        assert_eq!(add(&vec![3, -2], &vec![-3, 2]), Some(vec![0, 0]));
+
+        assert_eq!(add(&vec![1., 2.], &vec![4., 5.]), Some(vec![5., 7.]));
+        assert_eq!(add(&vec![3., -2.], &vec![-3., 2.]), Some(vec![0., 0.]));
+    }
+}
