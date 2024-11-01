68262cc98e098020ec9fc110cfceafa7001eb86d
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-01 19:20:55 +0600

    Supported vector corresponding sum

---
+++ math/linear_algebra/src/vector_sum.rs
@@ -0,0 +1,42 @@
+use std::ops::Add;
+
+pub fn vector_sum<T>(vectors: &[&[T]]) -> Option<Vec<T>>
+where
+    T: Copy + Default + Add<Output = T>,
+{
+    if vectors.len() == 0 || !vectors.iter().all(|v| v.len() == vectors[0].len()) {
+        return None;
+    }
+
+    let mut result = vec![T::default(); vectors[0].len()];
+    for v in vectors {
+        for (i, &value) in v.iter().enumerate() {
+            result[i] = result[i] + value;
+        }
+    }
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_vector_sum() {
+        let a = vec![1, 2, 3, 4];
+        let b = vec![2, 4, 6, 8, 10];
+        let vectors: Vec<&[i32]> = vec![&a, &b];
+        assert_eq!(vector_sum(&vectors), None);
+
+        let a = vec![1, 2, 3, 4, 5];
+        let b = vec![2, 4, 6, 8, 10];
+        let vectors: Vec<&[i32]> = vec![&a, &b];
+        assert_eq!(vector_sum(&vectors), Some(vec![3, 6, 9, 12, 15]));
+
+        let a = vec![1., 2., 3., 4., 5.];
+        let b = vec![2., 4., 6., 8., 10.];
+        let vectors: Vec<&[f64]> = vec![&a, &b];
+        assert_eq!(vector_sum(&vectors), Some(vec![3., 6., 9., 12., 15.]));
+    }
+}
