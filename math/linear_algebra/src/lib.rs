bec07b5d00d43c47bbe6aeb21017326b1f422f86
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-11-02 00:46:23 +0600

    Supported vector mean

--- math/linear_algebra/src/lib.rs
+++ math/linear_algebra/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod add;
 pub mod scalar_multiply;
 pub mod subtract;
+pub mod vector_mean;
 pub mod vector_sum;
---
+++ math/linear_algebra/src/vector_mean.rs
@@ -0,0 +1,49 @@
+use num::Num;
+
+pub fn vector_mean<T>(vectors: &[&[T]]) -> Option<Vec<T>>
+where
+    T: Copy + Num,
+{
+    if vectors.len() == 0 || !vectors.iter().all(|v| v.len() == vectors[0].len()) {
+        return None;
+    }
+
+    let mut result: Vec<T> = vec![T::zero(); vectors[0].len()];
+    let mut count: T = T::zero();
+    for i in 0..vectors[0].len() {
+        count = T::zero();
+        for v in vectors {
+            result[i] = result[i].add(v[i]);
+            count = count.add(T::one());
+        }
+    }
+
+    for i in 0..result.len() {
+        result[i] = result[i] / count;
+    }
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_vector_mean() {
+        let a = vec![1, 2, 3, 4];
+        let b = vec![2, 4, 6, 8, 10];
+        let vectors: Vec<&[i32]> = vec![&a, &b];
+        assert_eq!(vector_mean(&vectors), None);
+
+        let a = vec![2, 2, 4, 4, 6];
+        let b = vec![2, 4, 6, 8, 10];
+        let vectors: Vec<&[i32]> = vec![&a, &b];
+        assert_eq!(vector_mean(&vectors), Some(vec![2, 3, 5, 6, 8]));
+
+        let a = vec![2., 2., 4., 4., 6.];
+        let b = vec![2., 4., 6., 8., 10.];
+        let vectors: Vec<&[f64]> = vec![&a, &b];
+        assert_eq!(vector_mean(&vectors), Some(vec![2., 3., 5., 6., 8.]));
+    }
+}
